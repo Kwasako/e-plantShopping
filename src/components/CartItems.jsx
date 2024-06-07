b9193c4d7252326a64a5353e07daa36d8672c189
Kwasako <danburamayuba@gmail.com>
2024-06-07 23:28:12 +0100

    implemented the cartitem component

--- src/components/CartItems.jsx
+++ src/components/CartItems.jsx
@@ -1,15 +1,34 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { removeItemFromCart, increaseItemQuantity, decreaseItemQuantity, clearItems } from "./CreateSlice";
 
 
 function CartItems(){
     const cartItems = useSelector(state=>state.cart.cartItems)
     const totalAmount = cartItems.reduce((total, item)=>total + item.price * item.quantity, 0)
+    const dispatch = useDispatch()
+
+    function handleRemove(itemId){
+        dispatch(removeItemFromCart(itemId))
+
+    }
+
+    function handleDecrease(itemId){
+        dispatch(decreaseItemQuantity(itemId))
+    }
+
+    function handleIncrease(itemId){
+        dispatch(increaseItemQuantity(itemId))
+    }
+
+    function handleClear(){
+        dispatch(clearItems())
+    }
 
 
     return(
         <div className="container">
-            <div>{totalAmount && <div>Total Cart Amount: ${totalAmount}</div>} </div>
+            <div>{totalAmount ? <div>Total Cart Amount: ${totalAmount}</div>: ''} </div>
             {cartItems.map((item, index)=>(
                 <div className="row">
                     <div className="col-4">
@@ -20,19 +39,24 @@ function CartItems(){
                                 <div className="name">{item.name}</div>
                                 <div>${item.price}</div>
                                 <div className="quantity-section">
-                                    <button> - </button>
+                                    <button onClick={()=>handleDecrease(item.id)}> - </button>
                                     <span> {item.quantity} </span>
-                                    <button> + </button>
+                                    <button onClick={()=>handleIncrease(item.id)}> + </button>
                                 </div>
                                 <div className="price">${item.price * item.quantity}</div>
-                                <button className="delete-btn"> Delete</button>
+                                <button 
+                                    className="delete-btn" 
+                                    onClick={()=>handleRemove(item.id)}
+                                    > 
+                                    Delete
+                                </button>
                             </div>
                         </div>
                     </div>
                 </div>
                 </div> 
             ))}
-            <button className="clear-btn"> Clear </button>
+            <div>{totalAmount && <button className="clear-btn" onClick={handleClear}> Clear cart </button>}</div>
         </div>
     )
 }
--- src/components/CreateSlice.jsx
+++ src/components/CreateSlice.jsx
@@ -17,11 +17,37 @@ const CartSlice = createSlice({
             else{
                 state.cartItems.push({...action.payload, quantity:1})
             }
+        },
+
+        removeItemFromCart(state, action){
+            state.cartItems = state.cartItems.filter(item => item.id !== action.payload)
+        },
+
+        increaseItemQuantity(state, action){
+            const itemToIncrease = state.cartItems.find(item=> item.id === action.payload)
+            if (itemToIncrease){
+                itemToIncrease.quantity += 1
+            }
+        },
+
+        decreaseItemQuantity(state, action){
+            const itemToDecrease = state.cartItems.find(item => item.id === action.payload)
+            if (itemToDecrease && itemToDecrease.quantity > 1){
+                itemToDecrease.quantity -= 1
+            }
+        },
+
+        clearItems(state){
+            state.cartItems = []
         }
     }
 })
 
 export const {
-    addItemToCart
+    addItemToCart,
+    removeItemFromCart,
+    increaseItemQuantity,
+    decreaseItemQuantity,
+    clearItems
 } = CartSlice.actions
 export default CartSlice.reducer
